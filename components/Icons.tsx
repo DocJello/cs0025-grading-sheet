88efb0641d2a14564f52d0b5f5cfa4e684d92442
Angelo C. Arguson <acarguson@feutech.edu.ph>
2025-11-02 22:24:29 +0800

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export const DashboardIcon: React.FC<{ className?: string }> = ({ className }) => (
@@ -71,5 +70,11 @@ export const CallIcon: React.FC<{ className?: string }> = ({ className }) => (
 export const BellIcon: React.FC<{ className?: string }> = ({ className }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+    </svg>
+);
+
+export const WrenchScrewdriverIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.87-5.87m0 0L11.42 15.17m5.87-5.87L11.42 15.17m0 0L5.75 21A2.652 2.652 0 0 1 2 17.25l5.87-5.87m0 0L2 5.75A2.652 2.652 0 0 1 5.75 2l5.87 5.87m0 0L17.25 2A2.652 2.652 0 0 1 21 5.75l-5.87 5.87m0 0L11.42 15.17" />
     </svg>
 );
